7688c1b41ca1abb1d7b2c16063f4527da9e665d9
Jay15v <168007805+Jay15v@users.noreply.github.com>
2024-12-30 22:05:00 +0200

    x

--- app/view/pages/user/ViewHome.php
+++ app/view/pages/user/ViewHome.php
@@ -2,33 +2,34 @@
 class ViewHome {
     public function renderCarousel() {
         return '
-        <!-- Carousel -->
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="imgs/t.jpg" class="d-block w-100" alt="First Slide">
-                </div>
-                <div class="carousel-item">
-                    <img src="imgs/k.jpg" class="d-block w-100" alt="Second Slide">
-                </div>
-                <div class="carousel-item">
-                    <img src="imgs/t.jpg" class="d-block w-100" alt="Third Slide">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>';
+      <!-- Carousel -->
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../public/assets/imgs/t.jpg" class="d-block w-100" alt="First Slide">
+        </div>
+        <div class="carousel-item">
+            <img src="../public/assets/imgs/k.jpg" class="d-block w-100" alt="Second Slide">
+        </div>
+        <div class="carousel-item">
+            <img src="../public/assets/imgs/t.jpg" class="d-block w-100" alt="Third Slide">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>'
+;
     }
 
     public function renderPromotions() {
@@ -38,25 +39,25 @@ class ViewHome {
             <div class="row">
                 <div class="col-md-3">
                     <div class="promo-box">
-                        <img src="imgs/mdesign_bottle0.8_dimensions.png" alt="Promo 1">
+                        <img src="../public/assets/imgs/mdesign_bottle0.8_dimensions.png" alt="Promo 1">
                         <h5>Extra 10% off</h5>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="promo-box">
-                        <img src="imgs/a.png" alt="Promo 2">
+                        <img src="../public/assets/imgs/a.png" alt="Promo 2">
                         <h5>Shop like a boss</h5>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="promo-box">
-                        <img src="imgs/vvv.png" alt="Promo 3">
+                        <img src="../public/assets/imgs/vvv.png" alt="Promo 3">
                         <h5>Buy 5 Get 5% off</h5>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="promo-box">
-                        <img src="imgs/l.png" alt="Promo 4">
+                        <img src="../public/assets/imgs/l.png" alt="Promo 4">
                         <h5>Last Chance</h5>
                     </div>
                 </div>
